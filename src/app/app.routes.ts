4e3aee7d261ef94570f0e24e03e3c974eb649e4a
Elvan <elvan5566@gmail.com>
2025-08-12 09:39:35 +0800

    樣版完成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 // 前台
 import { FrontendComponent } from './layout/frontend/frontend.component';
@@ -20,6 +21,7 @@ import { MemberOrdersComponent } from './pages/member/member-orders/member-order
 import { OrdersComponent } from './pages/backend/orders/orders.component';
 // 後台
 import { BackendComponent } from './layout/backend/backend.component';
+import { SupplieslistComponent } from './pages/backend/supplieslist/supplieslist.component'
 export const routes: Routes = [
   // 根目錄自動轉到 /show 或 /show/home
   { path: '', pathMatch: 'full', redirectTo: 'show' },
@@ -60,8 +62,9 @@ export const routes: Routes = [
     component: BackendComponent,
     children: [
       // 沒有 backend 的 home，建議改成 orders
-      { path: '', redirectTo: 'orders', pathMatch: 'full'},
+      { path: '', redirectTo: 'orders', pathMatch: 'full' },
       { path: 'orders', component: OrdersComponent },
+      { path: 'supplieslist', component: SupplieslistComponent }
     ],
   },
   // 其它未知路徑都導回前台（可選）
---
+++ src/app/pages/backend/supplieslist/supplieslist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-supplieslist',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './supplieslist.component.html',
+  styleUrl: './supplieslist.component.scss'
+})
+export class SupplieslistComponent {
+
+}
