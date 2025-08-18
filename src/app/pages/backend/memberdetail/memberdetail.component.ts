6357fc7c21f1b723a907ba2ac27be48ed0683864
謝維澤 <xie8610@gmail.com>
2025-08-18 16:34:27 +0800

    後台

---
+++ src/app/pages/backend/memberdetail/memberdetail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memberdetail',
+  imports: [],
+  templateUrl: './memberdetail.component.html',
+  styleUrl: './memberdetail.component.scss'
+})
+export class MemberdetailComponent {
+
+}
---
+++ src/app/pages/backend/memberlist/memberlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memberlist',
+  imports: [],
+  templateUrl: './memberlist.component.html',
+  styleUrl: './memberlist.component.scss'
+})
+export class MemberlistComponent {
+
+}
