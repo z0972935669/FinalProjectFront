70e132c5b72ed2ae67d57b805bfc29d7b4d84ddc
Ken <ken199711@yahoo.com.tw>
2025-08-18 10:07:26 +0800

    08181006

---
+++ src/app/pages/backend/employeelistdetail/employeelistdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-employeelistdetail',
+  imports: [RouterModule],
+  templateUrl: './employeelistdetail.component.html',
+  styleUrl: './employeelistdetail.component.scss'
+})
+export class EmployeelistdetailComponent {
+
+}
