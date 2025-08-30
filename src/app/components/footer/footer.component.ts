1ba90e6ada4d9d7c4feaad89d4bd54da5d4717cf
Allen <z0972935669@gmail.com>
2025-08-30 22:49:05 +0800

    Go Top

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [FooterComponent],
   standalone: true,
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  showGoTop = false;
 
+  // 如果實際滾動的是 window，就用這個
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showGoTop = (window.pageYOffset || document.documentElement.scrollTop) > 300;
+  }
+
+  goTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
